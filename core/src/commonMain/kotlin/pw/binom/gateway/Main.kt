cb8f291ce498759dbbc468434f0f5256243c621c
subochev <caffeine.mgn@gmail.com>
2022-01-17 12:51:08 +0300

    init

--- core/src/commonMain/kotlin/pw/binom/gateway/Main.kt
+++ core/src/commonMain/kotlin/pw/binom/gateway/Main.kt
@@ -1,9 +1,11 @@
 package pw.binom.gateway
 
 import kotlinx.coroutines.*
+import pw.binom.ByteBuffer
 import pw.binom.io.httpClient.HttpClient
 import pw.binom.io.httpClient.create
 import pw.binom.io.httpServer.HttpServer
+import pw.binom.net.toURI
 import pw.binom.network.Network
 import pw.binom.network.NetworkAddress
 import pw.binom.network.bindUdp
@@ -19,10 +21,37 @@ fun main(args: Array<String>) {
         val udpPort = Dispatchers.Network.bindUdp(NetworkAddress.Immutable(host = "127.0.0.1", port = 36534))
         val client = HttpClient.create()
         launch {
-            while (currentCoroutineContext().isActive){
+            while (currentCoroutineContext().isActive) {
 
             }
         }
         println("OLOLO!")
     }
+}
+
+suspend fun runClient() {
+    val udpPort = Dispatchers.Network.bindUdp(NetworkAddress.Immutable(host = "127.0.0.1", port = 36534))
+
+    val buf = ByteBuffer.alloc(65535)
+    coroutineScope {
+        launch {
+            while (true) {
+                val addr = NetworkAddress.Mutable()
+                buf.clear()
+                udpPort.read(buf, addr)
+            }
+        }
+    }
+
+    val client = HttpClient.create()
+    val wsConnection = client.connect(method = "GET", uri = "http://192.168.88.27:8022".toURI())
+        .startWebSocket()
+    coroutineScope {
+        launch {
+            while (currentCoroutineContext().isActive) {
+
+            }
+        }
+    }
+
 }
